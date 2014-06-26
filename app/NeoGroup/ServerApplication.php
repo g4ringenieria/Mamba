ca576a5f89c24530efb2ea2ed2196c40a3de1d8c
Luis <luis.amengual@sitrack.com>
2014-06-26 17:51:54 -0300

    Subo cambios para que las aplicaciones de procesadores sean MVC

--- app/NeoGroup/ServerApplication.php
+++ app/NeoGroup/ServerApplication.php
@@ -2,27 +2,25 @@
 
 namespace NeoGroup;
 
-use NeoPHP\server\ServerApplication as FrameworkServerApplication;
+use NeoGroup\databases\ProductionDatabase;
 use NeoGroup\processors\CommandsProcessor;
 use NeoGroup\processors\ConnectionsCommandsProcessor;
 use NeoGroup\processors\ConnectionsDebugProcessor;
 use NeoGroup\processors\STD900DeviceProcessor;
 use NeoGroup\processors\TT8750DeviceProcessor;
-use NeoGroup\databases\ProductionDatabase;
+use NeoPHP\server\ServerApplication as FrameworkServerApplication;
 
 class ServerApplication extends FrameworkServerApplication
 {
-    const SERVER_PORT = 8000;
-    
     public function initialize ()
     {   
         parent::initialize();
         $this->setPort(8000);
-        $this->addProcessor(new ConnectionsDebugProcessor($this));
-        $this->addProcessor(new TT8750DeviceProcessor($this));
-        $this->addProcessor(new STD900DeviceProcessor($this));
-        $this->addProcessor(new CommandsProcessor($this));
-        $this->addProcessor(new ConnectionsCommandsProcessor($this));
+        $this->addProcessor(new ConnectionsDebugProcessor());
+        $this->addProcessor(new TT8750DeviceProcessor());
+        $this->addProcessor(new STD900DeviceProcessor());
+        $this->addProcessor(new CommandsProcessor());
+        $this->addProcessor(new ConnectionsCommandsProcessor());
     }
     
     /**
