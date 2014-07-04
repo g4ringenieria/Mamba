dc6600d79cd3ef7bba79214a02561560540cc184
Luis <luis.amengual@sitrack.com>
2014-07-04 09:20:54 -0300

    Subo correcciones de cosas que no son necesarias en el dashboard

--- app/NeoGroup/view/scripts/MainView.php
+++ app/NeoGroup/view/scripts/MainView.php
@@ -6,34 +6,21 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-        <!-- #CSS Links -->
-        <!-- Basic Styles -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->getBaseUrl(); ?>assets/bootstrap-3.1.0/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->getBaseUrl(); ?>assets/font-awesome-4.1.0/css/font-awesome.min.css">
-
-        <!-- SmartAdmin Styles : Please note (smartadmin-production.css) was created using LESS variables -->
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->getBaseUrl(); ?>css/smartadmin-production.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->getBaseUrl(); ?>css/smartadmin-skins.min.css">
-
-        <!-- #FAVICONS -->
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+        
         <link rel="shortcut icon" href="<?php echo $this->getBaseUrl(); ?>images/favicon/favicon.ico" type="image/x-icon">
         <link rel="icon" href="<?php echo $this->getBaseUrl(); ?>images/favicon/favicon.ico" type="image/x-icon">
-
-        <!-- #GOOGLE FONT -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
-
-        <!-- #APP SCREEN / ICONS -->
         <link rel="apple-touch-icon" href="<?php echo $this->getBaseUrl(); ?>images/splash/sptouch-icon-iphone.png">
         <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $this->getBaseUrl(); ?>images/splash/touch-icon-ipad.png">
         <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $this->getBaseUrl(); ?>images/splash/touch-icon-iphone-retina.png">
         <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $this->getBaseUrl(); ?>images/splash/touch-icon-ipad-retina.png">
-
-        <!-- iOS web-app metas : hides Safari UI Components and Changes Status Bar Appearance -->
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
-        <!-- Startup image for web apps -->
         <link rel="apple-touch-startup-image" href="<?php echo $this->getBaseUrl(); ?>images/splash/ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
         <link rel="apple-touch-startup-image" href="<?php echo $this->getBaseUrl(); ?>images/splash/ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
         <link rel="apple-touch-startup-image" href="<?php echo $this->getBaseUrl(); ?>images/splash/iphone.png" media="screen and (max-device-width: 320px)">
