7d9147662b4fdb3c418bf62b4a9236e2dd6babb3
Luis <luis.amengual@sitrack.com>
2014-07-17 18:25:31 -0300

    Subo modificaciones en el Logger

--- command.php
+++ command.php
@@ -1,5 +1,6 @@
 <?php
 
+chdir(__DIR__);
 require_once (__DIR__."/../NeoPHP2/sources/bootstrap.php");
 NeoPHP\ClassLoader::getInstance()->addIncludePath(__DIR__."/app");
 NeoGroup\CommandApplication::getInstance()->start();
