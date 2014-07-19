3d3a21370f4d4d88f1723902bc774581d5e81337
luis <luis@luis-VGN-N330FH>
2014-07-19 06:34:28 -0300

    Cambios para que los controladores vayan a controladores de NeoGroup

--- app/NeoGroup/controller/device/DeviceController.php
+++ app/NeoGroup/controller/device/DeviceController.php
@@ -4,11 +4,14 @@ namespace NeoGroup\controller\device;
 
 use NeoPHP\mvc\Controller;
 
-class DeviceController extends Controller
+abstract class DeviceController extends Controller
 {
     public function notifyPackageAction ($package)
     {
-        echo "313131303030313130313738";
+        $deviceId = hexdec(substr($package, 0, 4));
+        $datagramHexa = substr($package, 4);
+        $datagram = hex2bin($datagramHexa);
+        $this->getLogger()->info("Datagram received from \"" . ($deviceId>0?$deviceId:"?") . "\": " . $datagramHexa);
     }
 }
 
