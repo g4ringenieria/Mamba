77f9dce190f5f17617493e0e548e8d98856ca509
luis <luis@luis-VGN-N330FH>
2014-07-19 10:16:40 -0300

    Cambios para que los controladores vayan a controladores de NeoGroup

--- app/NeoGroup/controller/device/DeviceController.php
+++ app/NeoGroup/controller/device/DeviceController.php
@@ -6,12 +6,50 @@ use NeoPHP\mvc\Controller;
 
 abstract class DeviceController extends Controller
 {
-    public function notifyPackageAction ($package)
+    public function notifyPackageAction ($datagram)
     {
-        $deviceId = hexdec(substr($package, 0, 4));
-        $datagramHexa = substr($package, 4);
-        $datagram = hex2bin($datagramHexa);
-        $this->getLogger()->info("Datagram received from \"" . ($deviceId>0?$deviceId:"?") . "\": " . $datagramHexa);
+        $package = new DevicePackage($datagram);
+        $responsePackage = $this->notifyPackageReceived($package);
+        if ($responsePackage != null && $responsePackage instanceof DevicePackage)
+            print($responsePackage);
+        $this->getLogger()->info("Datagram received from \"" . ($package->getDeviceId() >0 ? $package->getDeviceId() : "?") . "\": " . substr($datagram, 4));
+    }
+    
+    public abstract function notifyPackageReceived (DevicePackage $package);
+}
+
+final class DevicePackage
+{
+    private $deviceId;
+    private $data;
+    
+    public function __construct($deviceId, $data=null)
+    {
+        if (isset($data))
+        {
+            $this->deviceId = $deviceId;
+            $this->data = $data;
+        }
+        else
+        {
+            $this->deviceId = hexdec(substr($deviceId, 0, 4));
+            $this->data = hex2bin(substr($deviceId, 4));
+        }
+    }
+    
+    public function getDeviceId()
+    {
+        return $this->deviceId;
+    }
+    
+    public function getData()
+    {
+        return $this->data;
+    }
+    
+    public function __toString()
+    {
+        return str_pad(dechex($this->deviceId), 4, "0", STR_PAD_LEFT) . bin2hex($this->data);
     }
 }
 
--- app/NeoGroup/controller/device/TT8750Controller.php
+++ app/NeoGroup/controller/device/TT8750Controller.php
@@ -4,6 +4,10 @@ namespace NeoGroup\controller\device;
 
 class TT8750Controller extends DeviceController
 {
+    public function notifyPackageReceived(DevicePackage $package)
+    {
+        return new DevicePackage(10064, "test");
+    }
 }
 
 ?>
