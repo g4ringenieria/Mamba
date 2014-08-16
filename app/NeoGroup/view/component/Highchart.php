46faebe785d2c83d75133038894573f20ee4beb6
martin <martin@darksideDesktop>
2014-08-16 18:46:31 -0300

    Agrego clases de highcharts

---
+++ app/NeoGroup/view/component/Highchart.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace NeoGroup\view\component;
+
+use NeoPHP\web\html\HTMLComponent;
+use NeoPHP\web\html\HTMLView;
+use NeoPHP\web\html\Tag;
+
+class Highchart extends HTMLComponent
+{
+
+    private $id;
+    private $titleAttributes;
+    private $xAxisAttributes;
+    private $yAxisAttributes;
+    private $attributes;
+    private $series;
+
+    public function __construct(HTMLView $view)
+    {
+        static $idCounter = 0;
+        $this->id = "highchart_" . ($idCounter++);
+        $this->view = $view;
+        $this->series = array();
+    }
+    
+    public function getTitleAttributes()
+    {
+        return $this->titleAttributes;
+    }
+
+    public function setTitleAttributes($titleAttributes)
+    {
+        $this->titleAttributes = $titleAttributes;
+    }
+
+    public function getXAxisAttributes()
+    {
+        return $this->xAxisAttributes;
+    }
+
+    public function setXAxisAttributes($xAxisAttributes)
+    {
+        $this->xAxisAttributes = $xAxisAttributes;
+    }
+
+    public function getYAxisAttributes()
+    {
+        return $this->yAxisAttributes;
+    }
+
+    public function setYAxisAttributes($yAxisAttributes)
+    {
+        $this->yAxisAttributes = $yAxisAttributes;
+    }
+        
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    public function setAttributes($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function getSeries()
+    {
+        return $this->series;
+    }
+
+    public function setSeries($series)
+    {
+        $this->series = $series;
+    }
+
+    public function addSerie($serie)
+    {
+        $this->series[] = $serie;
+    }
+
+    protected function createContent()
+    {
+        $div = new Tag("div", array("id" => $this->id));
+        return $div;
+    }
+
+    protected function onBeforeBuild()
+    {
+        $this->view->addScriptFile($this->view->getBaseUrl() . "assets/highcharts-4.0.3/js/highcharts.js");
+        $script = "$(function () {\n";
+        $script .= "    $('#{$this->id}').highcharts({\n";
+        $script .= "        chart: {";
+        $script .= "            zoomType: 'x'";
+        $script .= "        },";
+        if (!empty($this->titleAttributes)) {
+        $script .= "        title: {\n";
+        if (!empty($this->titleAttributes['text'])) {
+        $script .= "            text: '" . $this->titleAttributes['text'] . "',\n";
+        }
+        $script .= "        },\n";
+        }
+        if (!empty($this->xAxisAttributes)) {
+        $script .= "        xAxis: {\n";
+        if (!empty($this->xAxisAttributes['type'])) {
+        $script .= "            type: '" . $this->xAxisAttributes['type'] . "',\n";
+        }
+        $script .= "        },\n";
+        }
+        if (!empty($this->yAxisAttributes)) {
+        $script .= "        yAxis: {\n";
+        if (!empty($this->yAxisAttributes['title'])) {
+        $script .= "            title: {\n";
+        if (!empty($this->yAxisAttributes['title']['text'])) {
+        $script .= "                text: '" . $this->yAxisAttributes['title']['text'] . "'\n";
+        }
+        $script .= "            },\n";
+        }
+        $script .= "        },\n";
+        }
+        $script .= "        tooltip: {\n";
+//        $script .= "            valueSuffix: '°C'\n";
+        $script .= "        },\n";
+        $script .= "        legend: {\n";
+        $script .= "            layout: 'vertical',\n";
+        $script .= "            align: 'right',\n";
+        $script .= "            verticalAlign: 'middle',\n";
+        $script .= "            borderWidth: 0\n";
+        $script .= "        },\n";
+        $script .= "        series: [\n";
+        if (!empty($this->series)) {
+        foreach ($this->series as $serie) {
+        $script .= "            {\n";
+        $script .= "                type: '" . $serie->getType() . "',\n";
+        $script .= "                name: '" . $serie->getName() . "',\n";
+        $seriedata = $serie->getData();
+        if (!empty($seriedata)) {
+        $script .= "                data: [\n";
+        foreach ($seriedata as $data) {
+        $script .= "                    [" . implode(",", $data) . "],\n";
+        }
+        }
+        $script .= "                ]\n";
+        $script .= "            },\n";
+        }
+        }
+        $script .= "        ]\n";
+        $script .= "    });\n";
+        $script .= "});\n";
+        $this->view->addScript($script);
+    }
+
+}
+?>
+
--- app/NeoGroup/view/report/FuelReportsView.php
+++ app/NeoGroup/view/report/FuelReportsView.php
@@ -9,89 +9,119 @@ use NeoGroup\view\component\Form;
 use NeoGroup\view\component\MultiButton;
 use NeoGroup\view\component\Panel;
 use NeoGroup\view\component\SelectField;
+use NeoGroup\view\component\Highchart;
+use NeoGroup\view\component\HighchartSerie;
 use NeoGroup\view\SidebarSiteView;
-use NeoPHP\web\html\Tag;
 use NeoPHP\web\http\Parameters;
 
 class FuelReportsView extends SidebarSiteView
 {
+
     const OUTPUTTYPE_NONE = 0;
     const OUTPUTTYPE_GRID = 1;
     const OUTPUTTYPE_GRAPHIC = 2;
-    
+
     private $reports;
     private $outputType;
-    
+
     public function setReports($reports)
     {
         $this->reports = $reports;
     }
-    
-    public function setOutputType ($outputType)
+
+    public function setOutputType($outputType)
     {
         $this->outputType = $outputType;
     }
-    
+
     protected function buildSidebar($sidebar)
     {
         $sidebar->add($this->createFiltersForm());
     }
-    
+
     protected function buildContent($content)
     {
-        switch ($this->outputType)
-        {
+        switch ($this->outputType) {
             case self::OUTPUTTYPE_GRID:
-                $content->add ($this->createReportsGrid());
+                $content->add($this->createReportsGrid());
                 break;
             case self::OUTPUTTYPE_GRAPHIC:
-                $content->add ($this->createReportsGraphic ());
+                $content->add($this->createReportsGraphic());
                 break;
         }
     }
-    
-    protected function createFiltersForm ()
+
+    protected function createFiltersForm()
     {
         $parameters = Parameters::getInstance();
-        $holderSelector = new SelectField($this, array("placeholder"=>"Vehículo", "name"=>"holderId", "value"=>$parameters->holderId, "displayvalue"=>$parameters->holderId_text));
+        $holderSelector = new SelectField($this, array("placeholder" => "Vehículo", "name" => "holderId", "value" => $parameters->holderId, "displayvalue" => $parameters->holderId_text));
         $holderSelector->setSourceType(SelectField::SOURCETYPE_REMOTE);
         $holderSelector->setRemoteUrl($this->getBaseUrl() . "holders/?returnFormat=json&" . session_name() . "=" . session_id());
         $holderSelector->setDisplayTemplate("{id}: {name} {domain}");
         $dateFromPicker = new DatetimePicker($this);
-        $dateFromPicker->setAttributes(array("placeholder"=>"Fecha desde ...", "name"=>"dateFrom"));
-        $dateFromPicker->setValue(isset($parameters->dateFrom)? $parameters->dateFrom : (date("Y/m/d") . " 00:00:00"));
+        $dateFromPicker->setAttributes(array("placeholder" => "Fecha desde ...", "name" => "dateFrom"));
+        $dateFromPicker->setValue(isset($parameters->dateFrom) ? $parameters->dateFrom : (date("Y/m/d") . " 00:00:00"));
         $dateToPicker = new DatetimePicker($this);
-        $dateToPicker->setAttributes(array("placeholder"=>"Fecha hasta ...", "name"=>"dateTo"));
-        $dateToPicker->setValue(isset($parameters->dateTo)? $parameters->dateTo : (date("Y/m/d") . " 23:59:59"));
-        $button = new MultiButton("Buscar", array("class"=>"primary"));
-        $button->addAction("Tabla", array("action"=>"showReportsInTable"));
-        $button->addAction("Gráfico", array("action"=>"showReportsInGraphic"));
-        $form = new Form(array("method"=>"POST"));
+        $dateToPicker->setAttributes(array("placeholder" => "Fecha hasta ...", "name" => "dateTo"));
+        $dateToPicker->setValue(isset($parameters->dateTo) ? $parameters->dateTo : (date("Y/m/d") . " 23:59:59"));
+        $button = new MultiButton("Buscar", array("class" => "primary"));
+        $button->addAction("Tabla", array("action" => "showReportsInTable"));
+        $button->addAction("Gráfico", array("action" => "showReportsInGraphic"));
+        $form = new Form(array("method" => "POST"));
         $form->addField($holderSelector);
         $form->addField($dateFromPicker);
         $form->addField($dateToPicker);
         $form->add($button);
         return $form;
     }
-    
-    protected function createReportsGrid ()
+
+    protected function createReportsGrid()
     {
         $grid = new EntityTable();
-        $grid->addColumn ("Holder", "holder");
-        $grid->addColumn ("Equipo", "device_id");
-        $grid->addColumn ("Reporte", "reporttype_description");
-        $grid->addColumn ("Fecha", "date", function ($date) { return DateUtils::formatDate($date, $this->getSession()->userDateFormat, $this->getSession()->userTimeZone); } );
-        $grid->addColumn ("Tanque", "fuelTank");
-        $grid->addColumn ("Nivel comb.", "fuelLevel");
-        $grid->setEntities($this->reports);  
+        $grid->addColumn("Holder", "holder");
+        $grid->addColumn("Equipo", "device_id");
+        $grid->addColumn("Reporte", "reporttype_description");
+        $grid->addColumn("Fecha", "date", function ($date) {
+            return DateUtils::formatDate($date, $this->getSession()->userDateFormat, $this->getSession()->userTimeZone);
+        });
+        $grid->addColumn("Tanque", "fuelTank");
+        $grid->addColumn("Nivel (l)", "fuelLevel");
+        $grid->addColumn("Temperatura (°c)", "fuelTemperature");
+        $grid->setEntities($this->reports);
         return new Panel("Histórico de Reportes de combustible", $grid);
     }
-    
-    protected function createReportsGraphic () 
+
+    protected function createReportsGraphic()
     {
-        $content = new Tag("div", array(), "En construcción");
-        return new Panel("Histórico de Reportes de combustible", $content);
+        $highchart = new Highchart($this);
+        $highchart->setTitleAttributes(array('text' => "Histórico de Reportes de combustible"));
+        $highchart->setXAxisAttributes(array('type' => "datetime"));
+        $highchart->setYAxisAttributes(array('title' => array('text' => "l / °c")));
+        if (!empty($this->reports)) {
+            $fuelLevelData = array();
+            $fuelTemperatureData = array();
+            foreach ($this->reports as $report) {
+                $fuelLevelData[] = array("Date.UTC(" . date("Y,m,d,H,i,s", strtotime($report->getDate())) . ")", $report->getFuelLevel());
+                $fuelTemperatureData[] = array("Date.UTC(" . date("Y,m,d,H,i,s", strtotime($report->getDate())) . ")", $report->getFuelTemperature());
+            }
+            if (!empty($fuelLevelData)) {
+                $fuelLevelSerie = new HighchartSerie();
+                $fuelLevelSerie->setType(HighchartSerie::TYPE_LINE);
+                $fuelLevelSerie->setName("Nivel (l)");
+                $fuelLevelSerie->setData($fuelLevelData);
+                $highchart->addSerie($fuelLevelSerie);
+            }
+            if (!empty($fuelTemperatureData)) {
+                $fuelTemperatureSerie = new HighchartSerie();
+                $fuelTemperatureSerie->setType(HighchartSerie::TYPE_LINE);
+                $fuelTemperatureSerie->setName("Temperatura (°c)");
+                $fuelTemperatureSerie->setData($fuelTemperatureData);
+                $highchart->addSerie($fuelTemperatureSerie);
+            }
+        }
+        return new Panel("Histórico de Reportes de combustible", $highchart);
     }
+
 }
 
 ?>
